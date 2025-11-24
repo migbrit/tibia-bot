d6bd471e390ed77cf4d3aaaf65d9d18781d453c0
Miguel Brito <miguelfrbrito@gmail.com>
2025-11-24 13:38:17 -0300

    atualiza actions com func para identificar se o monstro esta morrendo

--- actions.py
+++ actions.py
@@ -2,23 +2,37 @@ import pyautogui as pg
 import constants
 
 def battle_empty():
-    return pg.locateOnScreen('imgs\\empty-battle.png', region=constants.BATTLE_REGION, confidence=0.8)
+    return pg.locateOnScreen('imgs\\empty-battle.png', grayscale=True, confidence=0.8)
 
 def pixelMatchesColor(position, rgb):
     x, y = position
     if pg.pixelMatchesColor(x, y, rgb): 
         return True
     return False
+
+def is_monster_losing_hp(pixel_pos, previous_color):
+    current_color = pg.pixel(*pixel_pos)
+    return current_color != previous_color
   
+def is_monster_targeted():
+    r, g, b = pg.pixel(*constants.POSITION_MONSTER_TARGET)
+    return r > 150 and g < 100 and b < 100 # Monster targeted when this pixel is red
+
 def kill_monster(): 
-    while battle_empty() == None:
-        pg.press('space')
-        print('monstro alvejado.')  
-        x, y = constants.POSITION_MONSTER_TARGET                         
-        while pg.pixelMatchesColor(x, y, constants.RED_TARGET_COLOR, tolerance=99): 
-            pg.sleep(1)
-            print('esperando monstro morrer...')
-        print('procurando outro monstro...')
+    while battle_empty() is None:
+        if not is_monster_targeted():
+            pg.press('space')
+            print('monster targeted.')  
+
+        while is_monster_targeted(): 
+            color = pg.pixel(*constants.POSITION_MONSTER_HP)
+            print('waiting monster to die...')
+            get_loot()
+            pg.sleep(2)
+            if not is_monster_losing_hp(constants.POSITION_MONSTER_HP, color):
+                pg.press('space')
+
+        print('searching another monster...')
 
 def go_to_flag(path, wait):
     flag = pg.locateOnScreen(path, region=constants.MINI_MAP_REGION, confidence=0.8)
@@ -58,6 +72,7 @@ def down_hole(should_down):
 def up_hole(should_up, anchor, plus_x, plux_y):
     if should_up:
         box = pg.locateOnScreen(anchor, region=constants.GAME_ACTION_REGION, confidence=0.8)
+        print('anchor -> ', box)
         if box:
             x, y = pg.center(box)
             pg.moveTo(x + plus_x, y + plux_y)
